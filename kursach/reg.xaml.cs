765832a5107bfa022c72125b61d32f7c137017ee
Родин Алексей Юрьевич <10200294@college.local>
2023-05-17 10:38:47 +0300

    ThirdDay

--- kursach/reg.xaml.cs
+++ kursach/reg.xaml.cs
@@ -45,6 +45,7 @@ namespace kursach
                 AppConnect.model0db.SaveChanges();
                 MessageBox.Show("Данные успешно добавлены!",
                     "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+                AppFrame.frameMain.GoBack();
             }
             catch
             {
